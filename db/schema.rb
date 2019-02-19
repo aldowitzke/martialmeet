ed41e4e12f76a76b289fb753a7ef83e55b7f2b88
joao gabriel <joaogabrielbarreto1337@gmail.com>
2019-02-19 12:19:44 -0300

    added index, show, new views

--- db/schema.rb
+++ db/schema.rb
@@ -38,7 +38,6 @@ ActiveRecord::Schema.define(version: 2019_02_19_112724) do
     t.datetime "updated_at", null: false
     t.string "name"
     t.string "city"
-    t.boolean "teacher?", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
