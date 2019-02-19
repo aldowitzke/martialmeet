7ff21c32f61c24f7e6ca1f776173a0ad496c398e
Leandro Souza <cartaparaleandro@gmail.com>
2019-02-19 17:13:41 -0300

    sign up form with all attrs and strong params on app controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
+  before_action :authenticate_user!, :configure_permitted_parameters, if: :devise_controller?
 
-  before_action :authenticate_user!
   include Pundit
 
   # Pundit: white-list approach.
@@ -13,6 +13,13 @@ class ApplicationController < ActionController::Base
   #   flash[:alert] = "You are not authorized to perform this action."
   #   redirect_to(root_path)
   # end
+
+  protected
+
+  # allows costumized strong params for user on DB
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :city, :teacher])
+  end
 
   private
 
