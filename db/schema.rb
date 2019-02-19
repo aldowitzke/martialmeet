cf00659c863bc317e9f10da2c8c608b739127461
Aldo Witzke <aldo_mwa@hotmail.com>
2019-02-19 14:13:41 -0300

    views done and fixed student_id bugs

--- db/schema.rb
+++ db/schema.rb
@@ -39,6 +39,7 @@ ActiveRecord::Schema.define(version: 2019_02_19_154610) do
     t.string "name"
     t.string "city"
     t.boolean "teacher?", default: false
+    t.boolean "teacher", default: false
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
