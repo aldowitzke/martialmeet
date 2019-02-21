e8f6fdddf4374bfa8cfac211aee879c80536e300
Aldo Witzke <aldo_mwa@hotmail.com>
2019-02-21 19:32:10 -0300

    centralized

--- db/schema.rb
+++ db/schema.rb
@@ -12,7 +12,6 @@
 
 ActiveRecord::Schema.define(version: 2019_02_21_181237) do
 
-
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -25,8 +24,8 @@ ActiveRecord::Schema.define(version: 2019_02_21_181237) do
     t.bigint "teacher_id"
     t.bigint "student_id"
     t.string "title"
-    t.string "photo"
     t.string "city"
+    t.string "photo"
     t.index ["student_id"], name: "index_offers_on_student_id"
     t.index ["teacher_id"], name: "index_offers_on_teacher_id"
   end
