1a31d1199786323f62e9d6d506553c31b62be91c
Aldo Witzke <aldo_mwa@hotmail.com>
2019-02-22 17:04:01 -0300

    fixed join button when you are logged out

--- app/helpers/offers_helper.rb
+++ app/helpers/offers_helper.rb
@@ -6,8 +6,10 @@ module OffersHelper
       tag.p("You have no student yet")
     elsif @offer.student
       tag.p("You joined this class")
-    else
+    elsif user_signed_in?
       link_to "Join Class", join_offer_path, class: "btn btn-primary", id: "join-button", method: :patch
+    else
+      link_to "Join Class", new_user_session_path, class: "btn btn-primary", id: "join-button"
       # link_to tag.i(class: "btn btn-primary", id: "join-button"), join_offer_path, method: :patch
     end
   end
