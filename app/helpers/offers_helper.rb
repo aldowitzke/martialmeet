798c634be883df73b26b4c3216028aa1b695169f
Aldo Witzke <aldo_mwa@hotmail.com>
2019-02-22 12:48:56 -0300

    show page updated

--- app/helpers/offers_helper.rb
+++ app/helpers/offers_helper.rb
@@ -1,13 +1,14 @@
 module OffersHelper
   def offer_status_icon
     if @offer.student && (@offer.teacher == current_user)
-      tag.i(class: "fas fa-walking")
+      tag.h3("#{@offer.student.name} joined your class!")
     elsif @offer.teacher == current_user
-      tag.i(class: "fas fa-door-open")
+      tag.p("You have no student yet")
     elsif @offer.student
-      tag.i(class: "fas fa-check")
+      tag.p("You joined this class")
     else
-      link_to tag.i(class: "fas fa-sign-in-alt"), join_offer_path, method: :patch
+      link_to "Join Class", join_offer_path, class: "btn btn-primary", id: "join-button", method: :patch
+      # link_to tag.i(class: "btn btn-primary", id: "join-button"), join_offer_path, method: :patch
     end
   end
 end
